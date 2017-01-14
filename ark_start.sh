5d0f25cd414e16431f346c705c37d392ffca0afc
Nexolight <snow.dream.ch@gmail.com>
2017-01-14 01:05:32 +0100

    Create ark_start.sh

---
+++ ark_start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+SESSION="Server name placeholder"
+MESSAGE="Server message placeholder"
+ADMINPW="changeme"
+PORT=7777
+QUERYPORT=27015
+RCONPORT=32330
+MPLAYERS=160
+
+/home/steam/steamcmd/steamapps/common/ARK/ShooterGame/Binaries/Linux/ShooterGameServer \
+"TheCenter\
+?listen\
+?SessionName=$SESSION\
+?Port=$PORT\
+?QueryPort=$QUERYPORT\
+?RCONEnabled=True\
+?RCONPort=$RCONPORT\
+?ServerAdminPassword=$ADMINPW\
+?MaxPlayers=$MPLAYERS\
+?Message=$MESSAGE" \
+-server \
+-USEALLAVAILABLECORES \
+-UseBattlEye \
+-vday \
+-webalarm \
+-nosteamclient &
+
+echo "Server should be available in approximately 10 minutes"
