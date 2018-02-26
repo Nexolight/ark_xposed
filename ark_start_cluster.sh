9fd1f75a5c04406c8663408552e35201e6bec2eb
nexolight <snow.dream.ch@gmail.com>
2018-02-26 05:57:56 +0100

    cluster autostart

--- ark_start_cluster.sh
+++ ark_start_cluster.sh
@@ -7,24 +7,29 @@ fi
 if ! pgrep -a ShooterGameSer | grep -Eo 'ShooterGameServer Ragnarok' &> /dev/null;then
 	echo "$(date) - Starting Ragnarok"
 	/home/ark/ark.ragnarok.sh &
-	sleep 120
+	sleep 15
 fi
 
 if ! pgrep -a ShooterGameSer | grep -Eo 'ShooterGameServer Aberration' &> /dev/null;then
 	echo "$(date) - Starting Aberration"
 	/home/ark/ark.aberration.sh &
-	sleep 120
+	sleep 15
 fi
 
 if ! pgrep -a ShooterGameSer | grep -Eo 'ShooterGameServer Scorched' &> /dev/null;then
 	echo "$(date) - Starting Scorched Earth"
         /home/ark/ark.scorchedearth.sh &
-        sleep 120
+	sleep 15
 fi
 
-#if ! pgrep -a ShooterGameSer | grep -Eo 'ShooterGameServer TheIsland' &> /dev/null;then
-#       echo "$(date) - Starting The-Island"
-#        /home/ark/ark.theisland.sh &
-#        sleep 120
-#fi
+if ! pgrep -a ShooterGameSer | grep -Eo 'ShooterGameServer TheIsland' &> /dev/null;then
+       echo "$(date) - Starting The-Island"
+        /home/ark/ark.theisland.sh &
+	sleep 15
+fi
+
+if ! pgrep -a ShooterGameSer | grep -Eo 'ShooterGameServer TheCenter' &> /dev/null;then
+       echo "$(date) - Starting The-Center"
+        /home/ark/ark.the-center.sh &
+fi
 
